1d5797619271e0ef166fa4e2db78a20ab0a5d4fc
aisohai <alekspawlik123@gmail.com>
2022-11-20 09:58:50 +0100

    lab2_zad1

---
+++ src/main/java/pl/edu/wszib/lab1/singleton/ConnectionEager.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.lab1.singleton;
+
+public final class ConnectionEager {
+
+    public final static ConnectionEager INSTANCE = new ConnectionEager();
+
+    private ConnectionEager() {
+    }
+
+}
