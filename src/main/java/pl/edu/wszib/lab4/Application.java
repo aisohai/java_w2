40da77e0670125aee83b324a8ffc4e502d8453a5
aisohai <alekspawlik123@gmail.com>
2023-01-15 09:20:58 +0100

    lab4zad1

--- src/main/java/pl/edu/wszib/lab4/Application.java
+++ src/main/java/pl/edu/wszib/lab4/Application.java
@@ -11,14 +11,9 @@ public class Application {
     public static void main(String[] args) {
         final CommandBus commandBus = new CommandBus(Map.of(
                 OrderCreateCommand.class, new OrderCreateCommandHandler()
-                // TODO task1: add new command - OrderItemAddCommand(orderId, productId, quantity)
-                OrderItemAddCommand.class, new OrderItemAddCommand()
         ));
         final OrderCreateCommand orderCreateCommand = new OrderCreateCommand(UUID.randomUUID().toString());
         commandBus.execute(orderCreateCommand);
-
-        final OrderItemAddCommand orderItemAddCommand = new OrderItemAddCommand(orderId, productId, quantity);
-        commandBus.execute(orderItemAddCommand)
 
     }
 }
